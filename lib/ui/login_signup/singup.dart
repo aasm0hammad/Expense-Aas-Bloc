47695df2b5412e2a1b94a537876d1a5ded9fa0a5
AASM0 <aas006900@gmail.com>
2025-03-26 23:57:08 +0530

    Form validation

--- lib/ui/login_signup/singup.dart
+++ lib/ui/login_signup/singup.dart
@@ -7,7 +7,6 @@ import 'package:ass_expense/ui/login_signup/register/user_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-
 class Signup extends StatefulWidget {
   @override
   State<Signup> createState() => _SignupState();
@@ -24,167 +23,225 @@ class _SignupState extends State<Signup> {
 
   TextEditingController confirmController = TextEditingController();
 
-  bool isLoading= false;
-  bool isPasswordVisible= false;
-  bool isConfirmPasswordVisible= false;
+  bool isLoading = false;
+  bool isPasswordVisible = false;
+  bool isConfirmPasswordVisible = false;
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.only(top: 50, left: 16, right: 16),
-        child: Column(
-          children: [
-            Center(
-                child: Text(
-              "SIGN UP",
-              style: TextStyle(fontFamily: "Poppins", fontSize: 25),
-            )),
-            Text(
-              "Create your Account ",
-              style: TextStyle(fontSize: 16, color: Colors.black),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            user(
-                "username",
-                Icon(
-                  Icons.person,
-                ),
-                nameController,
-                false),
-            SizedBox(
-              height: 20,
-            ),
-            user("email", Icon(Icons.email), emailController, false),
-            SizedBox(
-              height: 20,
-            ),
-            user("Moblie", Icon(Icons.call), phoneController, false),
-            SizedBox(
-              height: 20,
-            ),
-            user("password", Icon(Icons.password), passController, isPasswordVisible,
-                suffixIcon: InkWell(
-                    onTap: (){
-                      setState(() {
-                        isPasswordVisible= !isPasswordVisible;
-                      });
-                    },
-                    child: isPasswordVisible?Icon(Icons.visibility):Icon(Icons.visibility_off))),
-            SizedBox(
-              height: 20,
-            ),
-            user("Confirm password", Icon(Icons.password), confirmController, isConfirmPasswordVisible,
-                suffixIcon: InkWell(
-                    onTap: (){
-                      setState(() {
-                        isConfirmPasswordVisible= !isConfirmPasswordVisible;
-                      });
-                    },
-                    child: isConfirmPasswordVisible?Icon(Icons.visibility):Icon(Icons.visibility_off))),
-            SizedBox(
-              height: 50,
-            ),
-
-            BlocListener<RegisterBloc, RegisterState>(
-              listener: (context, state) {
-                if (state is RegisterLoadingState) {
-                  isLoading =true;
-                  setState(() {
-
-                  });
-
-                }
-                if (state is RegisterFailureState) {
-
-                  isLoading =false;
-                  setState(() {
-
-                  });
-                  ScaffoldMessenger.of(context)
-                      .showSnackBar(SnackBar(content: Text(state.errorMsg)));
-                }
-                if (state is RegisterSuccessState) {
-                  isLoading =false;
-                  Navigator.pop(context);
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text("Register Successful...")));
-                }
-              },
-              child: ElevatedButton(
-                onPressed: () async {
-                  context.read<RegisterBloc>().add(RegisterUserEvent(
-                      newUser: UserModel(
-                          uName: nameController.text,
-                          uEmail: emailController.text,
-                          uPhone: phoneController.text,
-                          uPassword: passController.text,
-                          uCreatedAt: DateTime.now()
-                              .microsecondsSinceEpoch
-                              .toString())));
-                },
-                child:isLoading? Row(
-                  children: [
-                    CircularProgressIndicator(),
-                    Text("Registering...")
-                  ],
-                ) :Text("Sign Up"),
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.pinkAccent,
-                    foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(11),
-                    ),
-                    padding: EdgeInsets.symmetric(horizontal: 100, vertical: 12)),
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Center(
+                  child: Text(
+                "SIGN UP",
+                style: TextStyle(fontFamily: "Poppins", fontSize: 25),
+              )),
+              Text(
+                "Create your Account ",
+                style: TextStyle(fontSize: 16, color: Colors.black),
               ),
-            ),
-            SizedBox(
-              height: 11,
-            ),
-            Text("OR"),
-            SizedBox(
-              height: 11,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, AppRoutes.ROUTE_HOME);
-              },
-              child: Text("Sign in with Google"),
-              style:ElevatedButton.styleFrom(
-                  backgroundColor: Colors.white,
-                  foregroundColor: Colors.pinkAccent,
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(11),
-                      side: BorderSide(color: Colors.pinkAccent)),
-                  padding:
-                  EdgeInsets.symmetric(horizontal: 70, vertical: 12)),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, AppRoutes.ROUTE_LOGIN);
-              },
-              child: Text.rich(TextSpan(
-                  text: "Already have an account? ",
-                  children: [
-                    TextSpan(
-                        text: "Login",
-                        style: TextStyle(color: Colors.pinkAccent))
-                  ])),
-            )
-          ],
+              SizedBox(
+                height: 50,
+              ),
+              user(
+                  "username",
+                  Icon(
+                    Icons.person,
+                  ),
+                  nameController,
+                  false, validator: (value) {
+                if (value!.isEmpty) {
+                  return "Username is required!";
+                } else {
+                  return null;
+                }
+              }),
+              SizedBox(
+                height: 20,
+              ),
+              user("email", Icon(Icons.email), emailController, false,
+                  validator: (value) {
+                if (value!.isEmpty) {
+                  return "Email is required!";
+                } else if (!RegExp(
+                        r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
+                    .hasMatch(value)) {
+                  return "Enter valid Email";
+                } else {
+                  return null;
+                }
+              }),
+              SizedBox(
+                height: 20,
+              ),
+              user("Moblie Number", Icon(Icons.call), phoneController, false,
+                  validator: (value) {
+                if (value!.isEmpty) {
+                  return " Mobile is required!..";
+                } else if (!RegExp(r'^[0-9]{10}$').hasMatch(value)) {
+                  return "Enter a valid 10-digit phone number";
+                } else {
+                  return null;
+                }
+              }),
+              SizedBox(
+                height: 20,
+              ),
+              user("password", Icon(Icons.password), passController,
+                  isPasswordVisible,
+                  suffixIcon: InkWell(
+                      onTap: () {
+                        setState(() {
+                          isPasswordVisible = !isPasswordVisible;
+                        });
+                      },
+                      child: isPasswordVisible
+                          ? Icon(Icons.visibility)
+                          : Icon(Icons.visibility_off)), validator: (value) {
+                if (value!.isEmpty) {
+                  return 'Password is required!..';
+                } else if (!RegExp(
+                        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$')
+                    .hasMatch(value)) {
+                  return "Please include Minimum 1 Upper case\nMinimum 1 lowercase\nMinimum 1 Numeric Number\nMinimum 1 Special Character";
+                } else {
+                  return null;
+                }
+              }),
+              SizedBox(
+                height: 20,
+              ),
+              user(
+                "Confirm password",
+                Icon(Icons.password),
+                confirmController,
+                isConfirmPasswordVisible,
+                suffixIcon: InkWell(
+                    onTap: () {
+                      setState(() {
+                        isConfirmPasswordVisible = !isConfirmPasswordVisible;
+                      });
+                    },
+                    child: isConfirmPasswordVisible
+                        ? Icon(Icons.visibility)
+                        : Icon(Icons.visibility_off)),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Confirm Pass field cannot be empty!";
+                  } else if (value != passController.text) {
+                    return "Password doesn't match!";
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              BlocListener<RegisterBloc, RegisterState>(
+                listener: (context, state) {
+                  if (state is RegisterLoadingState) {
+                    isLoading = true;
+                    setState(() {});
+                  }
+                  if (state is RegisterFailureState) {
+                    isLoading = false;
+                    setState(() {});
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text(state.errorMsg)));
+                  }
+                  if (state is RegisterSuccessState) {
+                    isLoading = false;
+                    Navigator.pop(context);
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Register Successful...")));
+                  }
+                },
+                child: ElevatedButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      context.read<RegisterBloc>().add(RegisterUserEvent(
+                          newUser: UserModel(
+                              uName: nameController.text,
+                              uEmail: emailController.text,
+                              uPhone: phoneController.text,
+                              uPassword: passController.text,
+                              uCreatedAt: DateTime.now()
+                                  .microsecondsSinceEpoch
+                                  .toString())));
+                    }
+                  },
+                  child: isLoading
+                      ? Row(
+                          children: [
+                            CircularProgressIndicator(),
+                            Text("Registering...")
+                          ],
+                        )
+                      : Text("Sign Up"),
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.pinkAccent,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(11),
+                      ),
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 100, vertical: 12)),
+                ),
+              ),
+              SizedBox(
+                height: 11,
+              ),
+              Text("OR"),
+              SizedBox(
+                height: 11,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, AppRoutes.ROUTE_HOME);
+                },
+                child: Text("Sign in with Google"),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.pinkAccent,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(11),
+                        side: BorderSide(color: Colors.pinkAccent)),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 70, vertical: 12)),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.pushNamed(context, AppRoutes.ROUTE_LOGIN);
+                },
+                child: Text.rich(TextSpan(
+                    text: "Already have an account? ",
+                    children: [
+                      TextSpan(
+                          text: "Login",
+                          style: TextStyle(color: Colors.pinkAccent))
+                    ])),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget user(String s, Icon preIcon, controller, bool obscureText,
-      {Widget? suffixIcon}) {
-    return TextField(
+      {Widget? suffixIcon, String? Function(String?)? validator}) {
+    return TextFormField(
+      validator: validator,
       obscureText: obscureText,
       controller: controller,
       decoration: InputDecoration(
